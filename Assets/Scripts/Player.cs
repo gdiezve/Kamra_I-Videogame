1805b21cf1eff1904203e0b88361d77ca842e286
Gina <georgina.diez@gmail.com>
2021-04-05 10:40:09 +0200

    Assets

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Transform foot;
+    public float footRadius;
+    public LayerMask floor;
+    public LayerMask enemy;
+    public bool isOnFloor;
+    public bool isOnEnemy; 
+
+    public HealthBar healthBar;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            TakeDamage(20);
+        }
+
+        isOnFloor = Physics2D.OverlapCircle(foot.position, footRadius, floor);
+        isOnEnemy = Physics2D.OverlapCircle(foot.position, footRadius, enemy);
+    }
+
+    void TakeDamage(int damage) {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    
+    public float initialPosition;
+    public float speed;
+    public float movementRange = 1f;
+    public bool lookingRight = false;
+    public GameObject player;
+    public GameObject foot;
+    public bool isDying = false;
+
+    Rigidbody2D rb;
+    Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = this.transform.position.x;
+        rb = GetComponent <Rigidbody2D>();
+        animator = GetComponent <Animator> ();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (lookingRight) {
+            if (this.transform.position.x > initialPosition + movementRange) {
+                lookingRight = false;
+                this.transform.localScale = new Vector3 (4.5f, 4.5f, 4.5f);
+            } else {
+                this.rb.velocity = new Vector3 (speed, rb.velocity.y, 0);
+                animator.SetFloat("speed", speed);
+            }
+        } else {
+            if (this.transform.position.x < initialPosition - movementRange) {
+                lookingRight = true;
+                this.transform.localScale = new Vector3 (-4.5f, 4.5f, 4.5f);
+            } else {
+                this.rb.velocity = new Vector3 (-speed, rb.velocity.y, 0);
+                animator.SetFloat("speed", speed);
+            }
+        }
+
+        if (isDying) {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D other) {
+        if (lookingRight) {
+            this.transform.localScale = new Vector3 (4.5f, 4.5f, 4.5f);
+            lookingRight = false;
+        } else {
+            this.transform.localScale = new Vector3 (-4.5f, 4.5f, 4.5f);
+            lookingRight = true;
+        }
+
+        if (player.gameObject.GetComponent <Player>().isOnEnemy == true) {
+            isDying = true;
+            animator.SetBool("isDying", isDying);
+        }
+    }
+}
