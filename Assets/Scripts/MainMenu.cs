d7a7e207569271537fecbb0ac4f336f4422e58f3
Gina <georgina.diez@gmail.com>
2022-01-02 20:58:40 +0100

    Adding music

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioListener audioListener;
+    
     public void PlayGame()
     {
+        audioListener.enabled = false;
         SceneManager.LoadScene("Level01");
     }
 
